e36839510839088e323b3eae56a3162ef2d1fa94
xy3 <a@xsq.pw>
2022-02-27 20:53:39 +0000

    Add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/xy3/photos"
 	"github.com/xy3/photos/handlers"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
 )
 
+func loadConfig() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	configFile := path.Join(cwd, "config.json")
+	log.Printf("Loading config from: %s\n", configFile)
+	file, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(file, &photos.Config)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeConfig() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	configFile := path.Join(cwd, "config.json")
+	log.Printf("Writing config to: %s\n", configFile)
+	configJson, err := json.Marshal(photos.Config)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(configFile, configJson, 0644)
+}
+
 func main() {
-	if err := photos.DbConnect(); err != nil {
+	err := loadConfig()
+	if err != nil {
+		log.Println("Failed to find or read a config.json file, using default config values instead")
+		_ = writeConfig()
+	}
+	if err = photos.DbConnect(); err != nil {
 		log.Fatal(err)
 	}
 	defer photos.DB.Close()
 
-	if err := photos.DbSetup(photos.DB); err != nil {
+	if err = photos.DbSetup(photos.DB); err != nil {
 		log.Fatal(err)
 	}
 
